52efb14d13c9c738fc432731e6307e3966fec557
PlayOsmo <abijithmg009@gmail.com>
2023-10-11 23:24:26 +0530

    4Th Oct class completed

--- 4oct2023/Lab007.py
+++ 4oct2023/Lab007.py
@@ -2,7 +2,43 @@
 #Store the values
 #like containers
 name = "Pramod"
+print(name)
 #Container with name name and store string Pramod
 #Variable name should be unique
 name = "Abijith"
 #it will overridden.
+print(name)
+
+#String is bunch of characters, A B C D- string is ""
+#Char - A B C D..... a, b, c, d, e "$"- Spl character
+
+#Data Types in Python
+#jave - int a =5, here is we don't need to tell the data type.
+#Python is dynamic type
+#x is an intiger type - int
+x = 5
+#x = "Amit"
+print(type(x))
+#String- bunch of character - str
+y = "words"
+print(type(y))
+#float - Decimal - float
+pi = 3.14
+print(type(pi))
+# Boolean - True or False - bool
+IsMale = True
+print(type(IsMale))
+
+#Numbers - Int and Float
+#Strings - Str
+#Booleans - Bool
+
+#Complex number
+#list
+#Tuples
+#Dictionary
+#Set
+#complex number - complex
+cn = 3+8j
+print(type(cn))
+
---
+++ 4oct2023/Lab008.py
@@ -0,0 +1,17 @@
+#how to take user input
+name = input("Enter your name")
+print(name)
+
+#Task for 4th Oct
+#Print the name of the user by taking from user input
+name = input("Enter your name")
+print("The user name is ", name)
+
+#Take First name and last name and print with Sep "-" and end with "\t"
+FirstName = input("Enter your First Name")
+LastName = input("Enter your Last Name")
+print(FirstName, LastName, sep="-", end="\t")
+
+#Take a user input as name and say with message, "You are welcome <Name> "to the Python class"
+name = input("Enter your name")
+print("You are welcome", name, "to the python class")
