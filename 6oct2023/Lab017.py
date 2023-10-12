bd4e6b38e9e1eeeaf825b3e161d2f889ff9300eb
PlayOsmo <abijithmg009@gmail.com>
2023-10-12 13:27:26 +0530

    Task completed

--- 6oct2023/Lab017.py
+++ 6oct2023/Lab017.py
@@ -32,6 +32,25 @@ print(n,"X 8 =\t", n*8)
 print(n,"X 9 =\t", n*9)
 print(n,"X 10 =\t",n*10)
 
+#Task 2 string functions
+'''
+len() -   Python String length can be determined by using built-in len() function.
+ascii() - Python ascii() function returns the string representation of the object. This function internally calls repr() function and before returning the representation string, escapes the non-ASCII characters using \x, \u or \U escapes.
+bool() - Python bool() function returns Boolean value for an object. The bool class has only two instances – True and False.
+bytearray() - Python bytearray() function returns a bytearray object that contains the array of bytes from the input source.
+bytes() - This function returns bytes object that is an immutable sequence of integers in the range 0 <= x < 256.
+ord() - Python ord() function takes string argument of a single Unicode character and return its integer Unicode code point value.
+enumerate() - Python enumerate function takes a sequence, and then make each element of the sequence into a tuple.
+float() - As the name says, python float() function returns a floating point number from the input argument.
+hash() - This function returns the hash value of the given object.
+id() - Python id() function returns the “identity” of the object. The identity of an object is an integer, which is guaranteed to be unique and constant for this object during its lifetime.
+int() - Python int() function returns an integer object from the specified input. The returned int object will always be in base 10.
+map() - Python map() function is used to apply a function on all the elements of specified iterable and return map object.
+print() - Python print() function is used to print data into console.
+slice() - Python slice() function returns a slice object representing the set of indices specified by range(start, stop, step).
+type() - This function returns the type of the object.
+'''
+
 
 
 
