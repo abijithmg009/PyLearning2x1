067a31bc6d43722866f0d7089bbb7decf0ab93c1
PlayOsmo <abijithmg009@gmail.com>
2023-10-12 16:28:41 +0530

    Task 9th oct added on Lab026

--- 9oct2023/Lab026.py
+++ 9oct2023/Lab026.py
@@ -1,2 +1,10 @@
 #Create a Program take 5 numbers from the user and add 1 or 2 duplicates. print the non-duplicate numbers
+num1 = input("Enter the number1\n")
+num2 = input("Enter the number2\n")
+num3 = input("Enter the number3\n")
+num4 = input("Enter the number4\n")
+num5 = input("Enter the number5\n")
+
+Set = {num1,num2,num3,num4,num5}
+print(Set)
 
